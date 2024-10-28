651b584bf4fb7b31f53de5da0cc5bf7b9ac008c3
phpwebsitejiyyo <websitejiyyo@gmail.com>
2024-10-28 23:40:03 +0530

    character component design created with Integrated API inside anime-details component

--- src/app/anime-detail/anime-characters/anime-characters.component.ts
+++ src/app/anime-detail/anime-characters/anime-characters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-anime-characters',
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimeCharactersComponent implements OnInit {
 
+  @Input('animeCharacters') animeCharacters: Array<any>;
+  @Input('animeImages') animeImages: any;
+  groupedCharacters: { [key: string]: Array<any> } = {};
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log("rendering character component...")
+    // console.log("rendering character component...")
+    for (var i = 0; i < this.animeCharacters.length; ++i) {
+      if (this.animeCharacters[i].role in this.groupedCharacters) {
+        this.groupedCharacters[this.animeCharacters[i].role].push(this.animeCharacters[i]);
+      }
+      else {
+        this.groupedCharacters[this.animeCharacters[i].role] = [];
+        this.groupedCharacters[this.animeCharacters[i].role].push(this.animeCharacters[i]);
+      }
+    }
   }
 
 }
